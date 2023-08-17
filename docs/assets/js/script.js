e08c8e99451a17b4bcbec8d9f95645e63ae14c3d
Jordan Masone <jordanmasone1@gmail.com>
2023-08-17 14:28:58 -0400

    Added beginning movement

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,3 +1,50 @@
 "use strict";
-console.log("Hello, world!");
-const value = 123;
+const addBtnEl = document.getElementById("add");
+const colorModeBtnEl = document.getElementById("color-mode");
+const settingsBoxEl = document.querySelector(".settings");
+const emojiSizes = { x: 55, y: 53 };
+let darkMode = localStorage.getItem("color-mode") === "true";
+let elementArray = [];
+class FloatingElement {
+    id;
+    htmlElement;
+    coordinates;
+    increments;
+    emoji;
+    speed;
+    constructor(id, emoji) {
+        this.id = id;
+        this.htmlElement = document.createElement("span");
+        this.coordinates = { x: 0, y: 0 };
+        this.increments = { x: 1, y: 1 };
+        this.emoji = emoji;
+        this.speed = 10;
+    }
+    init() {
+        elementArray.push(this);
+        this.htmlElement.innerText = this.emoji;
+        this.updateDOM();
+        document.body.appendChild(this.htmlElement);
+        return setInterval(() => this.movementInterval(), this.speed);
+    }
+    updateDOM() {
+        this.htmlElement.setAttribute("style", `left: ${this.coordinates.x}px; top: ${this.coordinates.y}px`);
+    }
+    movementInterval() {
+        this.coordinates = { x: this.coordinates.x + this.increments.x, y: this.coordinates.y + this.increments.y };
+        return this.updateDOM();
+    }
+}
+const toggleColorMode = () => {
+    localStorage.setItem("color-mode", darkMode.toString());
+    document.body.style.backgroundColor = darkMode ? "#0a0e12" : "white";
+    settingsBoxEl.style.color = darkMode ? "white" : "black";
+    colorModeBtnEl.setAttribute("class", `fa-solid fa-${darkMode ? "sun" : "moon"}`);
+};
+addBtnEl.addEventListener("click", () => {
+    return new FloatingElement(elementArray.length, "🪨").init();
+});
+colorModeBtnEl.addEventListener("click", () => {
+    darkMode = !darkMode;
+    return toggleColorMode();
+});
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,74 @@
-console.log("Hello, world!");
+// Interfaces
+interface Coordinates {
+    x: number;
+    y: number;
+}
 
-const value = 123;
+// HTML Elements
+const addBtnEl = document.getElementById("add") as HTMLElement;
+const colorModeBtnEl = document.getElementById("color-mode") as HTMLElement;
+const settingsBoxEl = document.querySelector(".settings") as HTMLElement;
+
+// Game Variables
+const emojiSizes: Coordinates = {x: 55, y: 53};
+let darkMode = localStorage.getItem("color-mode") === "true";
+let elementArray: FloatingElement[] = [];
+
+// Element Class
+class FloatingElement {
+    id: number;
+    htmlElement: HTMLSpanElement;
+    coordinates: Coordinates;
+    increments: Coordinates;
+    emoji: string;
+    speed: number;
+
+    constructor(id: number, emoji: string) {
+        this.id = id;
+        this.htmlElement = document.createElement("span");
+        this.coordinates = {x: 0, y: 0};
+        this.increments = {x: 1, y: 1};
+        this.emoji = emoji;
+        this.speed = 10;
+    }
+
+    init() {
+        // Add to array of FloatingElements
+        elementArray.push(this);
+
+        // Update DOM
+        this.htmlElement.innerText = this.emoji;
+        this.updateDOM();
+        document.body.appendChild(this.htmlElement);
+
+        // Start Movement
+        return setInterval(() => this.movementInterval(), this.speed)
+    }
+
+    updateDOM() {
+        this.htmlElement.setAttribute("style", `left: ${this.coordinates.x}px; top: ${this.coordinates.y}px`);
+    }
+
+    movementInterval() {
+        this.coordinates = {x: this.coordinates.x + this.increments.x, y: this.coordinates.y + this.increments.y}
+        return this.updateDOM();
+    }
+}
+
+
+// Helper Functions
+const toggleColorMode = () => {
+    localStorage.setItem("color-mode", darkMode.toString());
+    document.body.style.backgroundColor = darkMode ? "#0a0e12" : "white";
+    settingsBoxEl.style.color = darkMode ? "white" : "black";
+    colorModeBtnEl.setAttribute("class", `fa-solid fa-${darkMode ? "sun" : "moon"}`)
+}
+
+// Event Listeners
+addBtnEl.addEventListener("click", () => {
+    return new FloatingElement(elementArray.length, "🪨").init();
+});
+colorModeBtnEl.addEventListener("click", () => {
+    darkMode = !darkMode;
+    return toggleColorMode();
+});
